f517c861db2f5301991dff394792b9832ed28ebd
Jafer Al-Dakhily <jaferaldkly@gmail.com>
2022-11-10 09:49:51 +0300

    dynamic user created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar({searchTerm, setSearchTerm, user}) {
       </div>
       <div className="flex gap-3">
         {/* to={`user-profile/${user?.id}`} when I written the backend code */}
-      <Link to={`user-profile/:id`} className="hidden md:block">
+      <Link to={"user-profile/" + 1} className="hidden md:block">
         <img src={userImage} alt="user" className='w-14 h-12 rounded-lg' />
       </Link>
       <Link to="create-pin" className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'>
--- frontend/src/utils/data3.js
+++ frontend/src/utils/data3.js
@@ -1,15 +1,18 @@
-const images = [
+import userImage from '../assets/0018.jpg'
+const users = [
     {
       id:1,
       name: "Mohammed",
-      email : "mohammed"
+      email : "mohammed",
+      image : userImage
     },
     {
         id:2,
         name: "Morad",
-        email : "morad@mail.com"
+        email : "morad@mail.com",
+        image : userImage
     }
 
 ];
 
-export default images;
+export default users;

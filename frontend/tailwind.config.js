0cf24c178806166da078106f4f2e151e27aa52c2
jafer <jafer@mail.com>
2022-11-29 06:29:00 +0200

    added new changes in packages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
+  content: [
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {
       margin: {
